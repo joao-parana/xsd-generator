d7f1471a57ae09dec6776925aa4283201d1ce651
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-07-27 14:06:18 +0300

    Add more files to validation tests

--- validate.sh
+++ validate.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-file=./to-validate.xml
+#file=./validation/basic.xml
+#xmllint --schema schema.xsd $file
 
-xmllint --schema schema.xsd $file
+files[0]=./validation/basic.xml
+files[1]=./validation/simple-action-bar.xml
+files[2]=./validation/action-bar-items.xml
+files[3]=./validation/action-bar-nav-button.xml
+xmllint --schema schema.xsd ${files[*]}
+
